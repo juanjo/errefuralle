d30830765ae1dc7dda527e3d5bfe20fc02ddbde8
Juan Vidal <juan.vidal@sponsorpay.com>
2010-12-06 17:57:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erresauro::Application.config.secret_token = '0b1c5b0c467154028e4a3911eca6970ea4e9a6c93d6420521c913ff1afee63a3c428c7cc13b30ee09f0b3382af39a3f6645366932492fd3359035f41680c753d'
