fa6fc562e927143e93b4b139e0b065eb4edd817d
Juan Vidal <juan.vidal@sponsorpay.com>
2010-12-06 19:37:25 +0100

    new and edit snippets

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -10,4 +10,36 @@ class SnippetsController < ApplicationController
     @snippet = Snippet.find(params[:id])
   end
 
+  def new
+    @snippet = Snippet.new
+  end
+  
+  def create
+    @snippet.user = current_user
+    if @snippet.save
+      flash[:notice] = 'Snippet created successfully.'
+      redirect_to @snippet
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @snippet.update_attributes(params[:snippet])
+      flash[:notice] = 'Updated successfully.'
+      redirect_to @snippet
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @snippet.destroy
+    flash[:notice] = 'Snippet destroyed'
+    redirect_to snippets_url
+  end
+
 end
