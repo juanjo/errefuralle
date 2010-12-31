39ca535695606ad3767a0290ca3fa5d6a2e6eae3
Juan Jose Vidal <juanjo@binoid.com>
2010-12-31 13:56:18 +0100

    rendering popup twice fixed

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module Erresauro
     config.i18n.fallbacks = [:en]
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    #config.action_view.javascript_expansions[:defaults] = %w(rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
