722e0ba3fadd522f526e1cd6b75069909195cb34
Juan Jose Vidal <juanjo@binoid.com>
2011-01-01 23:39:34 +0100

    removing video and images markdown style from posts index

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,10 +1,23 @@
 require 'uv'
 module SnippetsHelper
-  def truncate_snippet(thought, wordcount)
+  
+  CLEANER_REGEXS = { :video => /!\[Video\]\(youtube:[a-zA-Z0-9\-\_]*\)/,
+                     :image => /!\[.*\]\(.+\ ".+"\)/ }
+  
+  def truncate_snippet(thought, wordcount, clean = false)
+    thought = regex_cleaner(thought) if clean
     thought.split[0..(wordcount - 1)].join(" ") + (thought.split.size > wordcount ? "..." : "")
   end
   
   def parse_uv(code, theme)
     Uv.parse(code, "xhtml", "r", true, theme).html_safe
   end
+  
+  def regex_cleaner(text)
+    SnippetsHelper::CLEANER_REGEXS.each_value do |value|
+      text = text.gsub(value, '')
+    end
+    return text
+  end
+  
 end
