b31b9247f4d0dd977add64b44eee8affa957637d
Fernando Guillen <fguillen.mail@gmail.com>
2011-01-09 17:58:59 +0100

    Not `validation_length_of` in the `Country.isonum` field
    For this isonum *724* it causes errors like this:
    
        ActiveRecord::RecordInvalid: La validación ha fallado: Isonum demasiado largo (el máximo es 6 caracteres)
    
    The `validation_length_of` has not any meaning in an integer field.
    I propose the `validates_inclusion_of` or remove this validation at all.

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,7 +5,9 @@ class Country < ActiveRecord::Base
   validates_presence_of :isonum, :iso2, :iso3, :name
   validates_uniqueness_of :isonum, :iso2, :iso3, :name
 
-  validates_length_of :isonum,  :maximum => 6
+  # validates_length_of :isonum,  :maximum => 6
+  validates_inclusion_of :isonum, :in => 0..999
+
   validates_length_of :iso2,    :is => 2
   validates_length_of :iso3,    :is => 3
 end
