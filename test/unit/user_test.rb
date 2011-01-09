6ab9a066e65941c1d2a546dee1c98c528b5e15da
Fernando Guillen <fguillen.mail@gmail.com>
2011-01-09 18:44:19 +0100

    `User` tests pass

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,61 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  subject { Factory(:user) }
+
+  should have_and_belong_to_many :roles
+  should have_many :snippets
+  should have_many :job_offers
+  should have_many :posts
+
+  should validate_presence_of   :username
+  should validate_uniqueness_of :username
+  should ensure_length_of(:username).is_at_least(2).is_at_most(10)
+  
+  context 'basics' do
+    setup do
+    end
+
+    should 'add registered role after create' do
+      user = Factory.build(:user)
+      assert_equal([], user.roles)
+      
+      user.save
+      assert_equal([Role[:registered]], user.roles)
+    end
+    
+    should 'scope confirmed' do
+      user_confirmed = Factory(:user)
+      user_confirmed.confirm!
+      user_not_confirmed = Factory(:user)
+      
+      assert_equal( [user_confirmed], User.confirmed)
+    end
+    
+    should 'scope unconfirmed' do
+      user_confirmed = Factory(:user)
+      user_confirmed.confirm!
+      user_not_confirmed = Factory(:user)
+      
+      assert_equal( [user_not_confirmed], User.unconfirmed)
+    end
+    
+    should 'return correctly if the user has the role asked or not' do
+      user_admin = Factory(:user)
+      user_admin.roles << Role[:admin]
+      user_not_admin = Factory(:user)
+      
+      assert( user_admin.role?(:admin) )
+      assert( !user_not_admin.role?(:admin) )
+    end
+    
+    should 'return correctly if the user can or not can do something' do
+      user_admin = Factory(:user)
+      user_admin.roles << Role[:admin]
+      user_not_admin = Factory(:user)
+
+      assert( user_admin.can?(:manage, Factory(:job_offer) ) )
+      assert( !user_not_admin.can?(:manage, Factory(:job_offer) ) )
+    end
   end
 end
