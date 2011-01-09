102bd2d1e9c345bdae14d9e418bb2ed6db2a09cc
Fernando Guillen <fguillen.mail@gmail.com>
2011-01-09 18:57:07 +0100

    `JobOffersController` tests pass

--- test/functional/job_offers_controller_test.rb
+++ test/functional/job_offers_controller_test.rb
@@ -1,79 +1,82 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 class JobOffersControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-    
-  def setup
-    @user = Factory(:user)
-    sign_in @user
-  end
 
-  def test_index
-    2.times { Factory(:job_offer) }
-    get :index
-    assert_template 'index'
-    assert_equal( 2, assigns(:job_offers).size )
-  end
-
-  def test_show
-    item = Factory(:item, :shop => @shop)
-    get :show, :shop_id => @shop.to_param, :id => item.to_param
-    assert_template 'show'
-    assert_not_nil( assigns(:item) )
-  end
-
-  def test_new
-    get :new, :shop_id => @shop.to_param
-    assert_template 'new'
-    assert_not_nil( assigns(:item) )
-  end
-
-  def test_create_invalid
-    Item.any_instance.stubs(:valid?).returns(false)
-    post :create, :shop_id => @shop.to_param
-    assert_template 'new'
-    assert_not_nil( flash[:alert] )
-  end
-
-  def test_create_valid
-    item_params = Factory.attributes_for(:item)
-    assert_difference "Item.count", 1 do
-      post :create, :shop_id => @shop.to_param, :item => item_params      
+  context 'basics' do
+    setup do
+      @user = Factory(:user)
+      @user.roles << Role[:admin]
+      @user.confirm!
+      sign_in @user
     end
-    assert_redirected_to backshop_item_url(@shop, assigns(:item))
-    assert_not_nil( flash[:notice] )
-  end
 
-  def test_edit
-    item = Factory(:item, :shop => @shop)
-    get :edit, :shop_id => @shop.to_param, :id => item.to_param
-    assert_template 'edit'
-    assert_not_nil( assigns(:item) )
+    should 'have an index' do
+      2.times { Factory(:job_offer, :user => @user) }
+      get :index
+      assert_template 'index'
+      assert_equal( 2, assigns(:job_offers).size )
+    end
+    
+    should 'have a show' do
+      job_offer = Factory(:job_offer, :user => @user)
+      get :show, :id => job_offer.to_param
+      assert_template 'show'
+      assert_not_nil( assigns(:job_offer) )
+    end
+    
+    should 'have a new' do
+      get :new
+      assert_template 'new'
+      assert_not_nil( assigns(:job_offer) )
+    end
+    
+    should 'return an alert if error in create' do
+      JobOffer.any_instance.stubs(:valid?).returns(false)
+      post :create
+      assert_template 'new'
+      assert_not_nil( flash[:alert] )
+    end
+    
+    should 'create success' do
+      job_offer_params = Factory.attributes_for(:job_offer)
+      assert_difference "JobOffer.count", 1 do
+        post :create, :job_offer => job_offer_params
+      end
+      assert_redirected_to job_offer_url( assigns(:job_offer) )
+      assert_not_nil( flash[:notice] )
+    end
+    
+    should 'have an edit' do
+      job_offer = Factory(:job_offer, :user => @user)
+      get :edit, :id => job_offer.to_param
+      assert_template 'edit'
+      assert_not_nil( assigns(:job_offer) )
+    end
+    
+    should 'return an alert if error in update' do
+      job_offer = Factory(:job_offer, :user => @user)
+      JobOffer.any_instance.stubs(:valid?).returns(false)
+      put :update, :id => job_offer.to_param
+      assert_template 'edit'
+      assert_not_nil( flash[:alert] )
+    end
+    
+    should 'update valid' do
+      job_offer = Factory(:job_offer, :user => @user)
+      put :update, :id => job_offer.to_param, :job_offer => { :title => 'new title' }
+      assert_redirected_to job_offer_url( assigns(:job_offer) )
+      job_offer.reload
+      assert_equal( 'new title', job_offer.title )
+      assert_not_nil( flash[:notice] )
+    end
+    
+    should 'have a destroy' do
+      job_offer = Factory(:job_offer, :user => @user)
+      delete :destroy, :id => job_offer.to_param
+      assert_redirected_to job_offers_url
+      assert !JobOffer.exists?(job_offer.id)
+      assert_not_nil( flash[:notice] )
+    end
   end
-
-  def test_update_invalid
-    item = Factory(:item, :shop => @shop)
-    Item.any_instance.stubs(:valid?).returns(false)
-    put :update, :shop_id => @shop.to_param, :id => item.to_param
-    assert_template 'edit'
-    assert_not_nil( flash[:alert] )
-  end
-
-  def test_update_valid
-    item = Factory(:item, :shop => @shop)
-    put :update, :shop_id => @shop.to_param, :id => item.to_param, :item => { :title => 'new title' }
-    assert_redirected_to backshop_item_url(@shop, assigns(:item))
-    item.reload
-    assert_equal( 'new title', item.title )
-    assert_not_nil( flash[:notice] )
-  end
-
-  def test_destroy
-    item = Factory(:item, :shop => @shop)
-    delete :destroy, :shop_id => @shop.to_param, :id => item.to_param
-    assert_redirected_to backshop_items_url( @shop )
-    assert !Item.exists?(item.id)
-    assert_not_nil( flash[:notice] )
-  end
-
 end
